e116685a672a7d13902dc5178145f24ebf41b270
zahra-y <121711411+zahra-y@users.noreply.github.com>
2022-12-31 12:40:02 +0330

    Update server.js
    change

--- server.js
+++ server.js
@@ -12,54 +12,7 @@ InstaClient.authBySessionId(yourSessionId)
 	.then(account => console.log(account))
 	.catch(err => console.error('err: '+err));
 
-const {logger} = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
-const cors = require('cors')
-const corsOptions = require('./config/corsOptions')
-const cookieParser = require("cookie-parser")
-const PORT = process.env.PORT || 3500
 
-const connectDB = require('./config/dbConn')
-const mongoose = require('mongoose')
-const {logEvents} = require('./middleware/logger')
 
-console.log(process.env.NODE_ENV)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-connectDB();
-
-app.use(logger)
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
-
-app.use('/' , express.static(path.join(__dirname, 'public')) )
-app.use('/', require('./routes/root') )
-
-app.use('/users', require('./routes/userRoutes'))
-app.use('/hashtags', require('./routes/hashtagRoutes'))
-
-app.all('*', (req,res)=>{
-    res.status(404)
-    if(req.accepts('html')){
-       res.sendFile(path.join(__dirname,'views','404.html')) 
-    }
-    else if(req.accepts('json')){
-        res.json({message : "404 Not Found"})
-    }
-    else{
-        res.type('txt').send('404 Text')
-    }
-    
-})
-
-app.use(errorHandler)
-
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-})
-
-mongoose.connection.on('error', err => {
-    console.log(err)
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
-})
