ab338263fc20f449d294c94ee8095014587309c0
zahra-y <121711411+zahra-y@users.noreply.github.com>
2022-12-31 12:45:10 +0330

    Update server.js
    ff

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
-require('dotenv').config()
 const express = require("express")
 const app = express()
-const path = require('path')
 
-const Insta = require('scraper-instagram');
-const InstaClient = new Insta();
-
-const yourSessionId = '6982271705%3ACqFOjDIwgfTBQH%3A13%3AAYdDXGyOLTu9bedjHmqhwU5bOMZnInMYpA_Ampwh2A';
-
-InstaClient.authBySessionId(yourSessionId)
-	.then(account => console.log(account))
-	.catch(err => console.error('err: '+err));
-
-
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
+app.use(express.json());
+app.get('/', (_,res) => res.send("works"));
